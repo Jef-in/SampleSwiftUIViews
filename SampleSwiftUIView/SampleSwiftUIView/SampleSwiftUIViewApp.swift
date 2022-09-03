db228219790d08dd78764cc5cd6288ad8effdc7f
Jef-in <jefinabduljaleel@gmail.com>
2022-09-03 10:32:50 +0530

    Project Setup Done

---
+++ SampleSwiftUIView/SampleSwiftUIView/SampleSwiftUIViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUIViewApp.swift
+//  SampleSwiftUIView
+//
+//  Created by Jefin on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUIViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
