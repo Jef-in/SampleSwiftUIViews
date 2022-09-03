10c3a2ad21d7e40073e61aeddd75b0dfae2091ba
Jef-in <jefinabduljaleel@gmail.com>
2022-09-03 11:01:32 +0530

    feat: Added Views to the contentView with Texts

--- SampleSwiftUIView/SampleSwiftUIView/ContentView.swift
+++ SampleSwiftUIView/SampleSwiftUIView/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 200.0)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImageView()
+                .offset(y: -100.0)
+                .padding(.bottom, -100.0)
+            VStack(alignment: .leading) {
+            Text("Louvre Museum")
+                .font(.title)
+                HStack {
+                    Text("Art Museum")
+                    Spacer()
+                    Text("Paris, France")
+                }
+                .font(.subheadline)
+                .foregroundColor(.gray)
+            }
             .padding()
+        }
+        Spacer()
     }
 }
 
